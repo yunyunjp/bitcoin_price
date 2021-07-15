6a389981bc0840a4d039c5dd225b2994a4e0083f
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-07-15 21:05:45 +0900

    Bitcoin価格の表示

--- js/main.js
+++ js/main.js
@@ -2,5 +2,16 @@ var app = new Vue({
   el: '#app',
   data: {
     bpi: null
+  },
+  mounted: function() {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response){
+      // console.log(response.data.bpi)
+      // console.log(response.data.bpi.USD.rate_float)
+      this.bpi = response.data.bpi
+    }.bind(this))
+    .catch(function(error){
+      console.log(error)
+    })
   }
 })
