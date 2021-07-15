276ac29f3bd682787b2ca2c0e8c3dbeaa495574f
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-07-15 21:27:54 +0900

    エラー、ローディング表示

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var app = new Vue({
   el: '#app',
   data: {
-    bpi: null
+    bpi: null,
+    hasError: false,
+    loading: true
   },
   mounted: function() {
     axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
@@ -12,7 +14,11 @@ var app = new Vue({
     }.bind(this))
     .catch(function(error){
       console.log(error)
-    })
+      this.hasError = true
+    }.bind(this))
+    .finally(function(){
+      this.loading = false
+    }.bind(this))
   },
   filters: {
     currencyDecimal(value) {
